14ec5b9ae15259d4abe72e7e45e5df5b87442823
W Berard <wberard@math.arizona.edu>
2017-05-26 16:44:40 -0700

    Add a view for all questions.

--- exam_creator/views.py
+++ exam_creator/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_questions(request):
+    questions = Question.objects.all()
+    return render(request, 'view_questions.html', {'questions': questions})
+
+
