53f61f53a628ce8109dbf9563e7f94d12662a758
W Berard <wberard@math.arizona.edu>
2017-05-26 16:10:41 -0700

    Initial definitions of Question and Topic models.

--- exam_creator/models.py
+++ exam_creator/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    latex = models.TextField()
+    source = models.CharField(max_length=100)
+    notes = models.TextField()
+    topics = models.ManyToManyField(Topic)
+
+
+
+class Topic(models.Model):
+    topic_name = models.CharField(max_length=100)
+    book_section = models.CharField(max_length=100)
