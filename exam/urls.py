0390f3d96b5283f7b121ff366bb24cd21cf6cc87
W Berard <wberard@math.arizona.edu>
2017-05-26 17:33:41 -0700

    Add questionslist url to urlpatterns.

--- exam/urls.py
+++ exam/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from exam_creator.views import questions_list
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^questionslist/$', questions_list),
 ]
