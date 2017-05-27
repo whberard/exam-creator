fb4582cec509e827d4c002932afefe8e2c2a607d
W Berard <wberard@math.arizona.edu>
2017-05-26 17:35:15 -0700

    Import Question and Topic models for the questions_list view.

--- exam_creator/views.py
+++ exam_creator/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Question, Topic
 
 # Create your views here.
 
-def view_questions(request):
+def questions_list(request):
     questions = Question.objects.all()
     return render(request, 'view_questions.html', {'questions': questions})
 
